54c60c0da5b33c76e066799bcb508c73b3f44fa3
Cjhay1324 <Gloc.poop@gmail.com>
2019-06-05 12:20:18 +0800

    smooth scroll done!

--- main.js
+++ main.js
@@ -1,26 +1,9 @@
-function smoothScroll(target, duration){
-    var target = document.querySelector(target);
-    var targetPosition = target.getBoundingClientRect().top;
-    var startPosition = window.pageYOffset;
-    var distance = targetPosition - startPosition;
-    var startTime = null;
-    
-    function animation(currentTime){
-        if(startTime === null) startTime = currentTime;
-        var timeElapsed = currentTime - startTime;
-        var run = ease(timeElapsed, startPosition, distance, duration);
-        window.scrollTo(0,run);
-        if(timeElapsed < duration) requestAnimationFrame(animation);
-    }
-    
-    function ease(t, b, c, d){
-        t /= d / 2;
-        if (t < 1) return c / 2 * t * t  + b;
-        t--;
-        return -c / 2 * (t * (t - 2) - 1) + b;
-    }
-    
-    requestAnimationFrame(animation);
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-smoothScroll('#skillset-section', 1000);
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
